5b06847bbfeb3a0b5c1b239a436e1120eeae86d8
Daniel Nachun <daniel.nachun@gmail.com>
2024-06-19 20:45:12 -0700

    Make shared library (#8)
    * create dynamic library instead of bundle
    
    * create dynamic library instead of bundle
    
    * make shared library
    
    * make shared library
    
    * make shared library

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,16 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-luarocks --tree ${PREFIX} build --lua-dir ${BUILD_PREFIX} --lua-version=5.1 --deps-mode=one --no-manifest lpeg-${PKG_VERSION}-1.rockspec CC=${CC} LD=${CC}
+DLLFLAGS=""
+if [[ ${target_platform} =~ .*osx.* ]]; then
+    DLLFLAGS="-Wl,-undefined,dynamic_lookup"
+fi
+
+make CC=${CC} LUADIR=${BUILD_PREFIX}/include/luajit-2.1 DLLFLAGS="${DLLFLAGS} -shared -fPIC" lpeg.so
+mkdir -p ${PREFIX}/lib/lua/5.1
+install -m 644 lpeg.so ${PREFIX}/lib/lua/5.1
+ln -sf ${PREFIX}/lib/lua/5.1/lpeg.so ${PREFIX}/lib/liblpeg${SHLIB_EXT} 
+
+mkdir -p ${PREFIX}/share/lua/5.1
+install -m 644 re.lua ${PREFIX}/share/lua/5.1
+install -m 644 test.lua ${PREFIX}/share/lua/5.1
